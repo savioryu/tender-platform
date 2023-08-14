e852a2aa16a59cd3d497225ab4c23221ccbd193d
bryanyu <bryanyu@tencent.com>
2023-08-14 21:19:21 +0800

    feat: 前端代码部署&文档优化

--- nest-web-server/src/main.ts
+++ nest-web-server/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-// import { join } from 'path';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,7 +15,7 @@ async function bootstrap() {
   };
 
   app.enableCors(corsOptions);
-  // app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.useStaticAssets(join(__dirname, '..', 'public/dist'));
 
   await app.listen(8000);
 }
